e8a2cf6488998eab6aa8b26216eac0b5192ba79e
angela-186155 <angela.186155@hastudents.us>
2023-10-18 18:06:26 -0700

    added page system

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,39 @@
-
-
+ready(function (){
+	
+	displayPage = document.getElementById("displayPage");
+	pages = {}
+	pages.homepage = function() { /*homepage template*/
+		page = document.getElementsByClassName("homepage template")[0].cloneNode(true);
+		page.classList.remove("template");
+		displayPage.textContent = "";
+		displayPage.appendChild(page);
+	}
+	pages.year_entries = function() { /*Year entries template*/
+		page = document.getElementsByClassName("year_entries template")[0].cloneNode(true);
+		page.classList.remove("template");
+		displayPage.textContent = "";
+		displayPage.appendChild(page);
+	}
+	pages.TESTPAGE = function() { /*TESTPAGE template*/
+		page = document.getElementsByClassName("TESTPAGE template")[0].cloneNode(true);
+		page.classList.remove("template");
+		displayPage.textContent = "";
+		displayPage.appendChild(page);
+	}
+	
+	/*function drkmdtoggle(event) {
+		document.body.classList.toggle("drkmd")
+	}
+	
+	document.getElementById("drkmdbtn").addEventListener("click", drkmdtoggle );*/
+		
+	function onRedir () {
+		if (location.hash === "#year_entries") pages.year_entries();
+		else if (location.hash === "#TESTPAGE") pages.TESTPAGE();
+		else pages.homepage();
+	}
+	
+	window.onhashchange = onRedir;
+	
+	window.onhashchange();
+});
